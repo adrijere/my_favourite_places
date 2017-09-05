307f3f25c161ae0773876a5304054d3d5edac40f
mathon_j <mathon_j@epitech.eu>
2017-09-05 21:07:02 +0200

    configuration for cookie session

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var bodyParser   = require('body-parser');
 var configDB = require('./config/database.js');
 var path = require('path');
+var cookieSession = require('cookie-session');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +13,8 @@ app.use(express.static(__dirname + '/public'));
 mongoose.connect(configDB.url);
 
 app.use(bodyParser()); // get information from html forms
+app.use(cookieSession({ name:'session', secret: "PingFlow" }));
+
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs'); // set up ejs for templating
