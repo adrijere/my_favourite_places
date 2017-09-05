a04a6a10643c05dd7618779df46735a8b3a7fb06
mathon_j <mathon_j@epitech.eu>
2017-09-05 20:04:06 +0200

    Verbose err on routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 // app/routes.js
 
-var User = require('../app/models/user');
+var user = require('../app/models/user');
 
 var mongoose = require("mongoose");
 
@@ -12,8 +12,8 @@ Airtable.configure({
 
 var base = Airtable.base('app8nTRhyaTqGhQTl');
 
-
 module.exports = function(app) {
+
   app.get('/', function(req, res) {
     var cities = [];
     base('Table 1').select({ view: "Grid view" }).eachPage(function page(records, fetchNextPage) {
@@ -29,7 +29,10 @@ module.exports = function(app) {
       fetchNextPage();
     }, function done(err) {
       if (err) { console.error(err); return; }
-      res.render('pages/index', { cities : cities, connected : false, user: null });
+      if (req.session.user)
+        res.render('pages/index', { cities : cities, connected : true, user: req.session.user });
+      else
+        res.render('pages/index', { cities : cities, connected : false, user: null });
     });
   });
 
@@ -54,9 +57,10 @@ module.exports = function(app) {
   });
 
   app.post('/login', function(req, res) {
-    User.findOne({ email: req.body.email, password: req.body.password}, function(err, _user) {
+    user.findOne({ email: req.body.email, password: req.body.password}, function(err, _user) {
       if (err || !_user) {
         console.log("Login failed!");
+        console.log("Err: " + err);
         return (res.render('pages/login'));
       }
       else {
@@ -74,6 +78,7 @@ module.exports = function(app) {
           fetchNextPage();
         }, function done(err) {
           if (err) { console.error(err); return; }
+          req.session.user = _user;
           res.render('pages/index', { cities: cities, connected: true, user: _user});
         });
       }
@@ -85,8 +90,8 @@ module.exports = function(app) {
   });
 
   app.post('/signup', function(req,res) {
-    var newuser = new User({email: req.body.email, password: req.body.password, favoris: []});
-    User.findOne({email: newuser.email}).exec(function(err, _user) {
+    var newuser = new user({email: req.body.email, password: req.body.password, favoris: []});
+    user.findOne({email: newuser.email}).exec(function(err, _user) {
       if (err || _user) {
         return (res.render('pages/login'));
       }
@@ -96,7 +101,42 @@ module.exports = function(app) {
             console.log(err);
         });
       }
+      req.session.user = newuser;
       res.render('pages/login');
     });
   });
+
+ app.get('/myaccount', function(req, res) {
+   res.render('pages/profile', {connected: true, user: req.session.user} );
+  });
+
+  app.get('/disconnect', function(req, res) {
+    user.findOne({email: req.session.user.email, password: req.session.user.password}).exec(function(err, _user) {
+      if (err || !_user) {
+        return (res.render('pages/login'));
+      }
+      else {
+        _user.favoris = req.session.user.favoris;
+        _user.save(function(err) {
+          if (err)
+            console.log(err);
+        });
+      }
+      req.session.user = null;
+      res.redirect('/');
+    });
+  });
+
+  app.post('/addfavourite/:city', function(req, res) {
+    req.session.user.favoris.push(req.params.city);
+    res.json(req.session.user.favoris);
+  });
+
+  app.post('/deletefavourite/:city', function(req, res) {
+    var i = req.session.user.favoris.indexOf(req.params.city);
+    if (i > -1)
+      req.session.user.favoris.splice(i, 1);
+    res.json(req.session.user.favoris);
+  });
+
 }
