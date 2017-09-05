680d164563cc1f82f7de24f5dca608d8fed80c54
mathon_j <mathon_j@epitech.eu>
2017-09-05 19:59:52 +0200

    FEATURE: Add something in your favourite, can delete the favourite and it's possible to do a research with the name of the city

--- public/lib/js/search-bar.js
+++ public/lib/js/search-bar.js
@@ -16,10 +16,54 @@
   });
 }); **/
 
-function login(showhide){
-  if(showhide == "show"){
-    document.getElementById('popupbox').style.visibility="visible";
-  }else if(showhide == "hide"){
-    document.getElementById('popupbox').style.visibility="hidden"; 
+$(document).ready(function() {
+  $( ".fav" ).click(function() {
+    if ($(this).hasClass("favourite")) {
+      $(this).css("color", "black");
+      $(this).removeClass("favourite");
+    }
+    else {
+      $(this).css("color", "#c6a30a");
+      $(this).addClass("favourite");
+      var city = $(this).parent().text();
+      console.log("city : " + city);
+      $.ajax({
+        url: "/addfavourite/" + city,
+        method: "POST",
+        success: function(data) {
+          console.log(data);
+        }
+      });
+    }
+  });
+
+  $( ".deleteFav" ).click(function() {
+    var city = $(this).parent().text();
+    $(this).parent().css("display", "none");
+    $.ajax({
+      url: "/deletefavourite/" + city,
+      method: "POST",
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  });
+
+});
+
+function search() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("places");
+  li = ul.getElementsByClassName('city');
+  
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByClassName("nameCity")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
 }
